64bfacaa9b6e23c81dede181c1c9849d4d7b4181
Hns Hlr <74055973+Hnshlr@users.noreply.github.com>
2023-07-08 11:29:13 +0100

    Update Utils.py
    Added a function to load calibration parameters for both Left and Right camera (Stereo application)

--- src/Utils.py
+++ src/Utils.py
@@ -14,13 +14,31 @@ def load_calib_v2(filepath):
     # Store all lines in a list of strings:
     with open(filepath, 'r') as f:
         lines = f.readlines()
+    # Extract the parameters from the first line:
     line0 = lines[0].split(' ')[1:]
-    # Extract the first two lines:
+    # Convert the parameters to a numpy array of floats:
     params = np.fromstring(' '.join(line0), dtype=np.float64, sep=' ')
-    # Reshape the array to a 3x4 matrix:
+    # Reshape the array to a 3x4 matrix (to obtain the projection matrix and the intrinsic matrix):
     P = np.reshape(params, (3, 4))
     K = P[0:3, 0:3]
     return K, P
+
+def load_calib_LR(filepath):
+    with open(filepath, 'r') as f:
+        lines = f.readlines()
+    # Extract the parameters from the first line:
+    line_left = lines[0].split(' ')[1:]
+    line_right = lines[1].split(' ')[1:]
+    # Convert the parameters to a numpy array of floats:
+    params_left = np.fromstring(' '.join(line_left), dtype=np.float64, sep=' ')
+    params_right = np.fromstring(' '.join(line_right), dtype=np.float64, sep=' ')
+    # Reshape the array to a 3x4 matrix (to obtain the projection matrix and the intrinsic matrix):
+    P_left = np.reshape(params_left, (3, 4))
+    P_right = np.reshape(params_right, (3, 4))
+    K_left = P_left[0:3, 0:3]
+    K_right = P_right[0:3, 0:3]
+    return K_left, P_left, K_right, P_right
+
 
 def load_poses(filepath):
     poses = []
