63f769f3845b864c75eeb82ea6a6c71af9d468a4
Hns Hlr <74055973+Hnshlr@users.noreply.github.com>
2023-07-20 17:52:39 +0100

    Removed the printing possibility for the x/y diff values calculating functions

--- src/Measurement.py
+++ src/Measurement.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import time
+import pandas as pd
 
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
@@ -12,9 +13,6 @@ def get_xy_values(gt_path, est_path):
     y_mean_diff = np.abs(np.round(np.mean([gt_path[i][1] - est_path[i][1] for i in range(len(gt_path))]), 2))
     x_max_diff = np.round(np.max([np.abs(gt_path[i][0] - est_path[i][0]) for i in range(len(gt_path))]), 2)
     y_max_diff = np.round(np.max([np.abs(gt_path[i][1] - est_path[i][1]) for i in range(len(gt_path))]), 2)
-    if print_values:
-        print("x_final_diff:", x_final_diff, " | y_final_diff:", y_final_diff, " | x_mean_diff:", x_mean_diff,
-              " | y_mean_diff:", y_mean_diff, " | x_max_diff:", x_max_diff, " | y_max_diff:", y_max_diff)
     return x_final_diff, y_final_diff, x_mean_diff, y_mean_diff, x_max_diff, y_max_diff
 
 # PLOT THE MONO, STEREO AND GT PATHS:
