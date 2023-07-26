bbec446c28d4277511b3bbb83fd4d02d989896a1
Hns Hlr <74055973+Hnshlr@users.noreply.github.com>
2023-07-26 17:10:31 +0100

    Added a function to Utils.py
    Added a function to Utils.py to resize an image given either an image width or an image height

--- src/Utils.py
+++ src/Utils.py
@@ -55,3 +55,18 @@ def form_transf(R, t):
     T[:3, :3] = R
     T[:3, 3] = t
     return T
+
+def ResizeWithAspectRatio(image, width=None, height=None, inter=cv2.INTER_AREA):
+    dim = None
+    (h, w) = image.shape[:2]
+
+    if width is None and height is None:
+        return image
+    if width is None:
+        r = height / float(h)
+        dim = (int(w * r), height)
+    else:
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    return cv2.resize(image, dim, interpolation=inter)
