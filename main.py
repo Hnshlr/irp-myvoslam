5b0d1256394bc64207dc4295d0d13a263868b553
Hans Haller <hanshaller@outlook.fr>
2023-08-11 20:47:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from src.VisualOdometry import *
 # SETTINGS=
 # ⌞ DATA=
 input_dir = "src/data/input/kitti/"
-skitti_indexes = ["S0", "S1", "S2", "S3", "S4", "S5", "S6", "S7", "S8", "S9", "S10"][6:8]
+skitti_indexes = ["S0", "S1", "S2", "S3", "S4", "S5", "S6", "S7", "S8", "S9", "S10"][6:7]
 kitti_indexes = ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10"]
 dataset_indexes = skitti_indexes
 # ⌞ METHOD=
@@ -30,7 +30,7 @@ saveData = False
 # MAIN:
 def main():
     # FOR EACH DATASET:
-    print("dataset,method,ss,fto,GRID_H,GRID_W,PATCH_MAX_FEATURES,ate,nc_ate")
+    print("dataset,method,ss,fto,GRID_H,GRID_W,PATCH_MAX_FEATURES,ate,nc_ate,ate_percent,nc_ate_percent")
     for dataset_path in [os.path.join(input_dir, dataset_index) for dataset_index in dataset_indexes]:
         for method in methods:
             # VISUAL ODOMETRY: Initialize the Visual Odometry class
@@ -51,8 +51,9 @@ def main():
                 )
             gt_path, est_path = vo.estimate_path(monitor=monitor, view=view)     # Estimate the path
             # MEASUREMENTS:
-            ate, nc_ate = get_ate(gt_path, est_path)    # Absolute Trajectory Error
-            print(f"{dataset_path.split('/')[-1]},{method},{do_SS},{do_FTO},{GRID_H},{GRID_W},{PATCH_MAX_FEATURES},{ate},{nc_ate}")
+            ate, nc_ate, ate_percent, nc_ate_percent = get_ate(gt_path, est_path)    # Absolute Trajectory Error
+            # PRINT:
+            print(f"{dataset_path.split('/')[-1]},{method},{do_SS},{do_FTO},{GRID_H},{GRID_W},{PATCH_MAX_FEATURES},{ate},{nc_ate},{ate_percent},{nc_ate_percent}")
 
 
 if __name__ == "__main__":
