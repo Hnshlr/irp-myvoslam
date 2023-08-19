cbc879d1df3b3c252eb89524b46fc7432fa074e8
Hans Haller <hanshaller@outlook.fr>
2023-08-19 20:00:53 +0100

    Updated the download link

--- scripts/wget-kitti.sh
+++ scripts/wget-kitti.sh
@@ -1,7 +1,7 @@
 mkdir -p src/data/input/kitti
 mkdir -p src/data/output/kitti
 
-wget "https://www.dropbox.com/scl/fi/7foz4er9snxed7equq1yo/skitti.zip?rlkey=zz687lyi9b0vju3byl0jnnqub&dl=1" -0 src/data/input/kitti/skitti.zip
+wget "https://www.dropbox.com/scl/fi/moukxxg7eeifv45w259e6/skitti.zip?rlkey=2xpoionnbai54vepbql8xm490&dl=1" -0 src/data/input/kitti/skitti.zip
 
 unzip src/data/input/kitti/skitti.zip -d src/data/input/kitti
 
