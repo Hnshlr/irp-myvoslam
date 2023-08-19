f3ce4937e766a3717a98dfb8dce5a82527f55167
Hans Haller <hanshaller@outlook.fr>
2023-08-20 00:15:02 +0100

    Re-ordered values

--- src/Measurement.py
+++ src/Measurement.py
@@ -26,7 +26,7 @@ def get_ate(gt_path, est_path):
     nc_ate = np.round(nc_ate, 2 - int(np.floor(np.log10(abs(nc_ate)))) - 1)
     ate_percent = np.round(ate / np.mean(np.linalg.norm(gt_path, axis=1)) * 100, 2)
     nc_ate_percent = np.round(nc_ate / np.mean(np.linalg.norm(gt_path, axis=1)) * 100, 2)
-    return ate, nc_ate, ate_percent, nc_ate_percent
+    return ate, ate_percent, nc_ate, nc_ate_percent
 
 
 # PLOT THE MONO, STEREO AND GT PATHS:
