2c1c0669780d74a97b17b862a424f1d4473428a0
Hans Haller <hanshaller@outlook.fr>
2023-08-21 16:00:15 +0100

    Added models to the downloadable data

--- scripts/curl-data-and-models.sh
+++ scripts/curl-data-and-models.sh
@@ -6,3 +6,11 @@ curl -L "https://www.dropbox.com/scl/fi/moukxxg7eeifv45w259e6/skitti.zip?rlkey=2
 unzip src/data/input/kitti/skitti.zip -d src/data/input/kitti
 
 rm src/data/input/kitti/skitti.zip
+
+mkdir -p src/models
+
+curl -L "https://www.dropbox.com/scl/fi/geevv3l7vv175zor79vjn/models.zip?rlkey=a9k4mfn1dl9lv371lhdvjmnn7&dl=1" -o src/models.zip
+
+unzip src/models.zip -d src/models
+
+rm src/models.zip
--- scripts/wget-data-and-models.sh
+++ scripts/wget-data-and-models.sh
@@ -6,3 +6,11 @@ wget "https://www.dropbox.com/scl/fi/moukxxg7eeifv45w259e6/skitti.zip?rlkey=2xpo
 unzip src/data/input/kitti/skitti.zip -d src/data/input/kitti
 
 rm src/data/input/kitti/skitti.zip
+
+mkdir -p src/models
+
+wget "https://www.dropbox.com/scl/fi/geevv3l7vv175zor79vjn/models.zip?rlkey=a9k4mfn1dl9lv371lhdvjmnn7&dl=1" -0 src/models.zip
+
+unzip src/models.zip -d src/models
+
+rm src/models.zip
